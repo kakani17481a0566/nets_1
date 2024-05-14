2539efde6c5a0acff4ccf7337b86e2ee2d3fb6ff
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2024-05-14 09:06:31 +0530

    u

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.getElementById("circleBtn").addEventListener("click", function() {
     this.classList.toggle("active");
   });
   
+
+  document.getElementById("circleBtn-1").addEventListener("click", function() {
+    this.classList.toggle("active");
+  });
+  
+
+
+  
